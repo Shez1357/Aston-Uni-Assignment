c14c555533b61d47f56c5b36c031f675c9426de6
Shez <shahryarquddus786123@gmail.com>
2026-03-25 18:14:33 +0000

    Code added

---
+++ config/db.php
@@ -0,0 +1,17 @@
+<?php 
+    $host = 'localhost';
+    $dbname = 'astoncv';
+    $user = 'root';
+    $pass = '';
+
+    try{
+        $pdo = new PDO(
+            "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+            $user,
+            $pass
+        );
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Could not connect to the database $dbname :" . $e->getMessage());
+    }
+?>
--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
+    <?php
+    session_start();
+    require 'config/db.php';
+    ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Aston Uni CV website</title>
 </head>
 <body>
-    <h1>Hello</h1>
-    <?php
-    $languages = array("PHP", "JavaScript", "Python", "Java");
-    foreach ($languages as $i){
-        echo $i . "<br>";
-    }
-    ?>
+    <h1>Aston University CV Website</h1>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href = "view_cv.php">View CVs</a>
+        
+    </nav>
 </body>
 </html>
