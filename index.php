380ab1f4c8cdf5877770ecf85f844bf7df307306
Shez <shahryarquddus786123@gmail.com>
2026-03-25 16:32:13 +0000

    File Structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aston Uni CV website</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <?php
+    $languages = array("PHP", "JavaScript", "Python", "Java");
+    foreach ($languages as $i){
+        echo $i . "<br>";
+    }
+    ?>
+</body>
+</html>
